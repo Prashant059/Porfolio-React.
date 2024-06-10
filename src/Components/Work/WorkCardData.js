c9df8210d016d375b9bda3e030825b950354aaee
Prashant Randive <prashant.randhive05@gmail.com>
2024-06-10 18:09:01 +0530

    Some new projects added

--- src/Components/Work/WorkCardData.js
+++ src/Components/Work/WorkCardData.js
@@ -1,6 +1,8 @@
 import firstPro from '../../assets/Img/firstPro.jpeg';
 import secondPro from '../../assets/Img/E-commerce.png';
 import thirdPro from '../../assets/Img/porfolio-img.PNG';
+import fourthPro from '../../assets/Img/Calculator.jpeg'
+import fivteethPro from '../../assets/Img/Password-Generator.jpeg'
 
 
 const projectCardData = [
@@ -17,12 +19,27 @@ const projectCardData = [
         text: "Transforming e-commerce landscapes with React.js—an agile JavaScript library. Its modular components and virtual DOM optimize rendering for rapid updates and a seamless user experience. Integration capabilities enable real-time updates, search functionalities, and personalized recommendations, empowering businesses to build visually appealing, high-performance online stores. React.js: the ideal choice for modern and competitive e-commerce development.",
         view: "https://e-commerce-84e7d.web.app/",
         source: "https://github.com/Prashant059/E-Commerece."
-    },{
+    },
+    {
         imgsrc: thirdPro,
         title: "React Portfolio",
         text: "Dynamic and engaging React Portfolio showcasing my proficiency in web development. With a modern and responsive design, this portfolio highlights my skills, projects, and achievements in a visually appealing manner. Explore my work and experience seamless navigation powered by React's robust capabilities.",
         view: "https://react-portfolio-3ea91.web.app/",
         source: "https://github.com/Prashant059/Porfolio-React."
+    },
+    {
+        imgsrc: fourthPro,
+        title: "Scientific Calculator",
+        text: "A comprehensive and user-friendly scientific calculator designed to handle a wide range of mathematical operations. This tool is perfect for students, engineers, scientists, and anyone needing accurate and efficient computations.",
+        view: "https://prashant059.github.io/Advance-Calculator/",
+        source: "https://github.com/Prashant059/Advance-Calculator"
+    },
+    {
+        imgsrc: fivteethPro,
+        title: "Random Password Generator",
+        text: "The core functionality of a JavaScript password generator involves generating passwords based on specified criteria. Users can typically set parameters such as the desired length of the password and whether to include uppercase letters, lowercase letters, numbers, and special characters.",
+        view: "https://prashant059.github.io/Password-Generator/",
+        source: "https://github.com/Prashant059/Password-Generator"
     }
 
 ]
