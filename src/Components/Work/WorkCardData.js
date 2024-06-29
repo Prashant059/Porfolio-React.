fd6e86a1a8fa06d299cf34f5fba967fd5873fcfd
Prashant Randive <prashant.randhive05@gmail.com>
2024-06-29 16:38:45 +0530

    Make changes in Project Section

--- src/Components/Work/WorkCardData.js
+++ src/Components/Work/WorkCardData.js
@@ -3,6 +3,10 @@ import secondPro from '../../assets/Img/E-commerce.png';
 import thirdPro from '../../assets/Img/porfolio-img.PNG';
 import fourthPro from '../../assets/Img/Calculator.jpeg';
 import fivteethPro from '../../assets/Img/Password-Generator.jpeg';
+import SixteethPro from '../../assets/Img/Nutition-Meter.jpeg';
+import SeventeethPro from '../../assets/Img/Weather-Mini.jpeg';
+import EigteethPro from '../../assets/Img/Tic-Tac-Toe.jpeg';
+import NineteethPro from '../../assets/Img/Alarm.jpeg';
 
 
 const projectCardData = [
@@ -67,6 +71,58 @@ const projectCardData = [
         text: "The core functionality of a JavaScript password generator involves generating passwords based on specified criteria. Users can typically set parameters such as the desired length of the password and whether to include uppercase letters, lowercase letters, numbers, and special characters.",
         view: "https://prashant059.github.io/Password-Generator/",
         source: "https://github.com/Prashant059/Password-Generator",
+        tech: {
+            "1st": "HTML",
+            "2nd": "CSS Code",
+            "3rd": "Javascript",
+            "4th": "DOM"
+        }
+    },
+    {
+        imgsrc: SixteethPro,
+        title: "Nutition Meter",
+        text: "Nutrition Meter is a React-based application designed to help users track their daily nutritional intake. It provides a user-friendly interface for logging meals and snacks, displaying a detailed breakdown of calories, macronutrients, and micronutrients consumed. Users can set personal dietary goals and monitor",
+        view: "https://prashant059.github.io/Nutrition-Meter/",
+        source: "https://github.com/Prashant059/Nutrition-Meter",
+        tech: {
+            "1st": "HTML",
+            "2nd": "CSS Code",
+            "3rd": "Javascript",
+            "4th": "DOM"
+        }
+    },
+    {
+        imgsrc: SeventeethPro,
+        title: "Mini-Weather-App",
+        text: "The Mini Weather App is a simple yet effective weather forecasting tool built using React and Vite. It provides real-time weather updates for any location, featuring a sleek, user-friendly interface. The app leverages modern web technologies to deliver fast and responsive performance.",
+        view: "https://prashant059.github.io/Mini-Weather-Ap/",
+        source: "https://github.com/Prashant059/Mini-Weather-Ap",
+        tech: {
+            "1st": "React",
+            "2nd": "Axios",
+            "3rd": "React-Loader",
+            "4th": "Font-Awesome"
+        }
+    },
+    {
+        imgsrc: EigteethPro,
+        title: "Mini-Weather-App",
+        text: `Create the classic Tic-Tac-Toe game using JavaScript to enhance your programming skills and have fun. This project involves setting up an HTML structure with a 3x3 grid and styling it with CSS. JavaScript is used to add interactivity, allowing players to click on grid cells to place their X or O.`,
+        view: "https://prashant059.github.io/Tic-Tac-Toe/",
+        source: "https://github.com/Prashant059/Tic-Tac-Toe",
+        tech: {
+            "1st": "HTML",
+            "2nd": "CSS Code",
+            "3rd": "Javascript",
+            "4th": "DOM"
+        }
+    },
+    {
+        imgsrc: NineteethPro,
+        title: "Alarm-App",
+        text: "Creating an alarm using JavaScript involves leveraging the Date object to set and manage future time0s, and dynamic DOM manipulation for user interactions. The script captures user input for the alarm date and time, then compares it to the current time to validate and set the alarm.",
+        view: "https://prashant059.github.io/Alarm-App/",
+        source: "https://github.com/Prashant059/Alarm-App",
         tech: {
             "1st": "HTML",
             "2nd": "CSS Code",
